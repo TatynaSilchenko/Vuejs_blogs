1995144601b911e5c631094ef85df09a425c44b2
Artur Yesipay <graydoe@yandex.by>
2020-12-14 16:01:23 +0300

    ***Add pagination

--- src/components/CharacterBlock.vue
+++ src/components/CharacterBlock.vue
@@ -1,11 +1,10 @@
 <template>
     <div class="character">
-
         <div class="avatar"><img :src="character.image" alt=""></div>
         <div class="info-wrapper">
-        <span>{{character.name}}</span>
-        <span>{{character.status}}-{{character.species}}</span>
-        <span>Last know location: {{character.location.name}}</span>
+            <span>{{character.name}}</span>
+            <span>{{character.status}}-{{character.species}}</span>
+            <span>Last know location: {{character.location.name}}</span>
         </div>
     </div>
 </template>
@@ -18,17 +17,10 @@
                 type: Object,
                 required: true
             }
-        },
-        methods:{
-            // getInfo(){
-            //     console.log(character)
-            // }
         }
     }
 </script>
 
 <style scoped>
-.character{
-    display: flex;
-}
+
 </style>
--- src/views/Blogs.vue
+++ src/views/Blogs.vue
@@ -4,31 +4,60 @@
         <CharacterBlock v-for="character in characters"
                         :key="character.id"
                         :character="character"/>
+        <paginate
+                v-model="currentPage"
+                :page-count="pages"
+                :page-range="3"
+                :margin-pages="2"
+                :click-handler="clickCallback"
+                :prev-text="'Prev'"
+                :next-text="'Next'"
+                :container-class="'pagination'"
+                :page-class="'page-item'">
+        </paginate>
     </div>
 </template>
 
 <script>
     import CharacterBlock from "../components/CharacterBlock";
+    import Paginate from 'vuejs-paginate';``
 
     export default {
         name: "Blogs",
-        components: {CharacterBlock},
+        components: {CharacterBlock,Paginate},
         data() {
             return {
                 currentPage: 1
             }
         },
-        created() {
-            this.$store.dispatch('fetchCharacters', this.currentPage)
-        },
         computed: {
             characters() {
                 return this.$store.getters['getCaractersByPage'](this.currentPage)
+            },
+            pages(){
+                return this.$store.state.pages.pages
             }
+        },
+        methods: {
+            clickCallback(pageNum) {
+                console.log(pageNum)
+            }
+        },
+        watch: {
+            currentPage: {
+                handler(page){
+                    this.$store.dispatch('fetchCharacters', page)
+                },
+                immediate:true
+            }
+
         }
     }
 </script>
 
 <style scoped>
-
+    .pagination {
+    }
+    .page-item {
+    }
 </style>
